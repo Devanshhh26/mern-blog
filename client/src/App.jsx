ebac2769a1df7baaa2e5fbba3eb3281594eb0b96
Devansh Mehta <devanshmehta26@gmail.com>
2024-02-03 18:27:12 +0530

    Pages Creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-    <div className='text-3xl'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
