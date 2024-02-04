294019557334e698c489a30c57aa543ecc1b71db
Devansh Mehta <devanshmehta26@gmail.com>
2024-02-04 18:44:08 +0530

    Error Handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,11 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'
-
-export const signup= async(req,res)=>{
+import { errorHandler } from '../utils/error.js';
+export const signup= async(req,res,next)=>{
     const  {username, email, password}=req.body;
     if(!username    ||  !password   ||  !email)
     {
-        return res.status(400).json({
-            message:"All fields are required"
-        })
+        next(errorHandler(400,'All fields are required'));
     }
     const hashedPassword=bcryptjs.hashSync(password,10);
     const newUser=new User({
@@ -21,9 +19,7 @@ export const signup= async(req,res)=>{
     }
     catch(err)
     {
-        res.status(500).json({
-            message:err.message
-        })
+        next(err);
     }
     
 }
