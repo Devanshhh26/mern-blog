e1e34e74f37f8f73f68185023d64c94cb60f9ed4
Devansh Mehta <devanshmehta26@gmail.com>
2024-02-22 23:01:41 +0530

    Completed About and Project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibol text-center my-7'>About Devansh's Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Devansh's Blog is a blog that I created to share my thoughts and ideas with the world.
+              I am a software engineer and I love to write about my experiences and things that I have learned.
+              I hope you enjoy reading my blog.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
