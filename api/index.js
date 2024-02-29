5a750d7003dd68d3b3b635c1a21b3bb441e71fea
Devansh Mehta <devanshmehta26@gmail.com>
2024-02-29 17:50:19 +0530

    Hosting

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ mongoose.connect(process.env.MONGO)
 
 const app= express();
 
+const __dirname=path.resolved();
+
 app.use(express.json());
 app.use(cookieParser());
 app.listen(3000,()=>{
@@ -29,6 +32,12 @@ app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname,'client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+});
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
